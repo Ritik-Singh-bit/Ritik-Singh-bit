e43cb1a64dcf0d4a25822df9a6aafd26e0a7e11d
Ritik Singh <ritik36366@gmail.com>
2024-11-17 13:40:25 +0530

    Update instagramBot.py

--- instagramBot.py
+++ instagramBot.py
@@ -1,9 +1,14 @@
+# first import the instabot
 from instabot import Bot 
 bot=Bot()
+# enter the user name and password 
 bot.login(username=" _.hutiyapa_", password ='bn,mgjhg')
+# enter the username that we want to follow 
 bot.follow('ig_ritik.singh')
-
+# past the path of photo
 bot.upload_photo("path of photo") 
+# enter username to unfollow
 bot.unfollow("username ")
+# now enter the msg you want to send 
 bot.send_message("i love u")
 
