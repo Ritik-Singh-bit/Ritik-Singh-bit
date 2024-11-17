c1cf6186f20464d40e773f3c6181cfe0991d82ec
Ritik Singh <ritik36366@gmail.com>
2024-11-17 13:05:54 +0530

    Add files via upload
    It's a  simple Instagram bot

---
+++ instagramBot.py
@@ -0,0 +1,9 @@
+from instabot import Bot 
+bot=Bot()
+bot.login(username=" _.hutiyapa_", password ='bn,mgjhg')
+bot.follow('ig_ritik.singh')
+
+bot.upload_photo("path of photo") 
+bot.unfollow("username ")
+bot.send_message("i love u")
+
